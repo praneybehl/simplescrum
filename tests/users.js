c8a25458645e41b29dee9cbffadc2bd948212df8
bigdcoder <dustin.engle@gmail.com>
2014-08-07 16:04:48 -0700

    User and account tests are passing.

--- tests/users.js
+++ tests/users.js
@@ -17,50 +17,13 @@ suite('Users', function() {
     });
 
     client.eval(function() {
-      Meteor.users.insert({
-        _audits: [],
-        _account: 'Test',
-        emails: [{address: 'test@email.com', verified: false}],
+      Accounts.createUser({
+        email: 'test@email.com',
+        password: 'password',
         profile: {
           fname: 'John',
           lname: 'Doe'
-        },
-        created_by: 'TestUser',
-        date_created: new Date()
-      });
-    });
-  });
-
-  // try the fix for the timeout
-  test('creating a user (test)', function(done, server, client) {
-    // observe the collection on the server and test
-    server.eval(function() {
-      Meteor.users.find().observe({
-        added: addNewUser
-      });
-
-      function addNewUser(user) {
-        emit('user', user);
-      }
-      emit('ready');
-    }).once('user', function(user) {
-      // test our user
-      assert.equal(user.profile.fname, 'John');
-      done();
-    }).once('ready', function() {
-      // insert our new user
-      client.eval(function() {
-        Meteor.users.insert({
-          _audits: [],
-          _account: 'Test',
-          emails: [{address: 'test@email.com', verified: false}],
-          profile: {
-            fname: 'John',
-            lname: 'Doe'
-          },
-          created_by: 'TestUser',
-          date_created: new Date()
-        });
+        }
       });
     });
   });
