5b90dbda5f48a003b88f55d20fd6bf8cef3e6123
bigdcoder <dustin.engle@gmail.com>
2014-08-08 16:41:30 -0700

    Fixed what tab shows in userList based on path.

--- client/helpers/userList.js
+++ client/helpers/userList.js
@@ -1,3 +1,14 @@
+Template.userList.rendered = function() {
+  // if team is the path then show
+  if (Router.current().path.indexOf('teams') !== -1) {
+    // show teams
+    self.$('.nav-tabs li a[href="#teams"]').tab('show');
+  } else {
+    // default to users
+    self.$('.nav-tabs li a[href="#users"]').tab('show');
+  }
+};
+
 Template.userList.helpers({
   getRoles: function(roles) {
     return roles.join(', ');
