4081ac1b09b83dfede76756603f1da2840e5ab10
bigdcoder <dustin.engle@gmail.com>
2014-08-08 10:28:00 -0700

    Updated home page and controller to include summary blocks.

--- client/controllers/HomeController.js
+++ client/controllers/HomeController.js
@@ -1,5 +1,21 @@
 HomeController = RouteController.extend({
   onBeforeAction: function() {
     Session.set('layout', 2);
+  },
+  data: function() {
+    return {
+      recentUsers: function() {
+        return Meteor.users.find({}, {limit: 10, sort: {date_created: 'desc'}}).fetch();
+      },
+      recentProjects: function() {
+        return Projects.find({}, {limit: 10, sort: {date_created: 'desc'}}).fetch();
+      },
+      recentSprints: function() {
+        return Sprints.find({}, {limit: 10, sort: {date_created: 'desc'}}).fetch();
+      },
+      recentStories: function() {
+        return Stories.find({}, {limit: 10, sort: {date_created: 'desc'}}).fetch();
+      }
+    };
   }
 });
